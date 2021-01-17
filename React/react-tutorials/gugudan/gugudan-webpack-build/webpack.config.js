de330e53151dbd42760fd72bf98e20de4aa5d69f
0hee0 <53266682+0hee0@users.noreply.github.com>
2021-01-17 21:01:20 +0900

    Add optional settings

--- React/react-tutorials/gugudan/gugudan-webpack-build/webpack.config.js
+++ React/react-tutorials/gugudan/gugudan-webpack-build/webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   name: 'gugudan-setting',
-  mode: 'development',
-  devtool: 'eval',
+  mode: 'development',  // production
+  devtool: 'eval',      // hidden-source-map
   resolve: {
     extensions: ['.js', '.jsx'],
   },
@@ -12,19 +13,35 @@ module.exports = {
     app: ['./gugudan']
   },
 
-  module: {
+  // watch: true,
+
+  module: { // Loader
     rules: [{
       test: /\.jsx?/,
       loader: 'babel-loader',
       options: {
-        presets: ['@babel/preset-env', '@babel/preset-react'],
+        presets: [
+          ['@babel/preset-env', {
+            // preset-env 설정 중요
+            targets: {
+              // Reference. https://github.com/browserslist
+              browsers: ['> 1% in KR'],
+            },
+          }], 
+          '@babel/preset-react',
+        ],
         plugins: ['@babel/plugin-proposal-class-properties'],
-      }
+      },
     }],
   },
+
+  plugins: [
+    new webpack.LoaderOptionsPlugin({ debug: true }),
+  ],
 
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'app.js'
   },
+
 };
